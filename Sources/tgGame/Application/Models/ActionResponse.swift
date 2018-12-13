725e49d8cc932cd94603eaea6c6fec8630da7635
Alexey Salangin <a7ls@ya.ru>
2018-12-13 13:12:28 +0300

    Detail description

--- Sources/tgGame/Application/Models/ActionResponse.swift
+++ Sources/tgGame/Application/Models/ActionResponse.swift
@@ -10,8 +10,9 @@ import Foundation
 struct ActionResponse {
     let nick: String
     let action: ActionType
-    let action_status: Int
-    let hits: Int
+    let action_status: ActionStatus
+    let hits: Int?
+    let target: String?
 }
 
 extension ActionResponse: Decodable { }
@@ -19,7 +20,7 @@ extension ActionResponse: Decodable { }
 extension ActionResponse {
     var localizedDescription: String {
         if let fighter = Battlefield.shared.fighters[nick] {
-            return "\(fighter.username) \(action.localizedDescription)"
+            return "\(fighter.username) \(action.localizedDescription)\(action_status.localizedDescription)"
         }
         return "\(nick) \(action.localizedDescription)"
     }
---
+++ Sources/tgGame/Application/Models/ActionStatus.swift
@@ -0,0 +1,44 @@
+//
+//  ActionStatus.swift
+//  Async
+//
+//  Created by Алексей on 13/12/2018.
+//
+
+import Foundation
+
+enum ActionStatus: Int {
+    case success = 0
+    case confused = 1
+    case missed = 2
+    case slip = 3
+    case tired = 10
+    case dodged = 11
+    case watched = 20
+    case died = 30
+}
+
+extension ActionStatus: Codable { }
+
+extension ActionStatus {
+    var localizedDescription: String {
+        switch self {
+        case .success:
+            return " успешно"
+        case .confused:
+            return ", но не того"
+        case .missed:
+            return ", но не попал"
+        case .slip:
+            return " и промахнулся, так как цель увернулась"
+        case .tired:
+            return " и устал"
+        case .dodged:
+            return ", хотя его не били"
+        case .watched:
+            return " и наблюдает"
+        case .died:
+            return " и умер"
+        }
+    }
+}
